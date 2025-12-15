f20e159e2cc0fb243aa899745745c31ff536c1da
anaximanderomiletus-byte <anaximanderomiletus@gmail.com>
2025-12-14 23:16:13 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify("AIzaSyB89-8rjw3tX8wBVCwJ5COB2xCCM9I0MUc"),
-        'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyB89-8rjw3tX8wBVCwJ5COB2xCCM9I0MUc")
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: './',  // <--- ADD THIS LINE HERE
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify("AIzaSyB89-8rjw3tX8wBVCwJ5COB2xCCM9I0MUc"),
+      'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyB89-8rjw3tX8wBVCwJ5COB2xCCM9I0MUc")
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
